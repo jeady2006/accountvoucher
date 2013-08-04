f218f049b52f3835e121f4b50dad287f6ceb3d57
yangweili <yangweili@192.168.0.101>
2013-08-04 20:46:08 +0800

    a

---
+++ src/jing/util/fields/DBFields.java
@@ -0,0 +1,19 @@
+package jing.util.fields;
+
+public class DBFields {
+	public static final String TABLE = "Table";
+	public static final String RETURN_FIELDS = "ReturnFields";
+	public static String QUERY_CONDITION = "QueryCondition";
+	public static String UPDATE_FIELDS = "UpdateFields";
+	
+
+	// InputVoucher
+	public static final String SEQ = "Seq";
+	public static final String VOUCHER_DATE = "VoucherDate";
+	public static final String REF = "Ref";
+	public static final String MEMBER = "Member";
+	public static final String CODE = "Code";
+	public static final String AMOUNT = "Amount";
+	public static final String CC = "CC";
+	public static final Object WBS = "WBS";
+}
